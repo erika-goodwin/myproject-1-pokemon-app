906f43e4fa78254d4a21ae17954adbd8fe58621f
erika-goodwin <shellvoerika@gmail.com>
2021-12-10 07:33:22 -0800

    with error

--- src/App.js
+++ src/App.js
@@ -9,26 +9,28 @@ function App() {
   const [loadMore, setLoadMore] = useState(
     "https://pokeapi.co/api/v2/pokemon?limit=20"
   );
-
   const getAllPokemons = () => {
     axios
       .get(loadMore)
       .then((res) => {
         setLoadMore(res.data.next);
         const createPokemonObject = (pokemonData) => {
+          console.log("PokoeonData: ", pokemonData);
           pokemonData.map(async (pokemon) => {
             const res = await fetch(
               `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
             );
             const data = await res.json();
+            console.log("response data:", data);
             // const res = await axios.get(
             //   `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`
             // );
             // const data = await res.data;
-            setPokemon20((currentList) => [...currentList, data]);
+            setPokemon20(...pokemon20, data);
+            // setPokemon20((currentList) => [...currentList, data]);
           });
         };
-        createPokemonObject(res.data.result);
+        createPokemonObject(res.data.results);
       })
       .catch();
   };
@@ -47,62 +49,3 @@ function App() {
 }
 
 export default App;
-
-//NEXT PAGE SYSTEM
-// function App() {
-//   console.log("App render:");
-//   const [pokemon, setPokemon] = useState([]);
-//   const [currentPageUrl, setCurrentPageUrl] = useState(
-//     "https://pokeapi.co/api/v2/pokemon"
-//   );
-
-//   const [nextPageUrl, setNextPageUrl] = useState();
-//   const [prevPageUrl, setPrevPageUrl] = useState();
-//   const [loading, setLoading] = useState(true);
-
-//   const getAllPokemons = () => {
-//     // let cancel;
-//     axios
-//       .get(currentPageUrl)
-//       // .get(currentPageUrl, {
-//       //   cancelToken: new axios.CancelToken((c) => (cancel = c)),
-//       // })
-//       .then((res) => {
-//         setLoading(false);
-//         setNextPageUrl(res.data.next);
-//         setPrevPageUrl(res.data.previous);
-//         setPokemon(res.data.results.map((p) => p.name));
-
-//       })
-//       .catch((err) => console.log(err));
-//   };
-
-//   useEffect(() => {
-//     setLoading(true);
-
-//     getAllPokemons();
-
-//     // return () => cancel.cancel();
-//   }, [currentPageUrl]);
-
-//   function gotoNextPage() {
-//     setCurrentPageUrl(nextPageUrl);
-//   }
-//   function gotoPrevPage() {
-//     setCurrentPageUrl(prevPageUrl);
-//   }
-
-//   if (loading) return "Loading...";
-
-//   return (
-//     <>
-//       <PokemonList pokemon={pokemon} />
-//       <Pagination
-//         gotoNextPage={gotoNextPage ? gotoNextPage : null}
-//         gotoPrevPage={gotoPrevPage ? gotoPrevPage : null}
-//       />
-//     </>
-//   );
-// }
-
-// export default App;
--- src/component/PokemonList.js
+++ src/component/PokemonList.js
@@ -2,9 +2,10 @@ import React from "react";
 
 export default function PokemonList({ pokemon }) {
   console.log("Pokemon List:", pokemon);
+  const pokemonName = pokemon.name;
   return (
     <div>
-      {pokemon.map((p) => (
+      {pokemon && pokemon.map((p) => (
         <div key={p}>{p}</div>
       ))}
     </div>
